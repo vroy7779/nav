c6cf04137bc3724cdb48c1b9991a93b522e5b32c
vroy7779 <vroyui@gmail.com>
2018-05-07 01:21:22 -0500

    everything
    yay

---
+++ portfolio/navbar/nav2.js
@@ -0,0 +1,15 @@
+document.write("<nav class='navbar navbar-default'>");
+document.write("<div class='container-fluid'>")
+document.write("<div class='navbar-header'>"); 
+document.write("<a class='navbar-brand' href='index.html'><img src='img/logo.PNG' class='img-responsive'><\/a>");
+document.write("</div>");
+document.write("<ul class='nav navbar-nav navbar-right'>"); 
+document.write("<li><a href='index.html'>Home<\/a></li>");
+document.write("<li><a href='resume.html'>Resume<\/a></li>");
+document.write("<li><a href='https://www.behance.net/vroyuib9fe'>Portfolio<\/a></li>"); 
+document.write("<li class='active'><a href='about.html'>More About me<\/a></li> "); 
+document.write("<li><a href='contact.html'>Contact<\/a><\/li>");
+document.write("</ul>");
+document.write("</li>"); 
+document.write("</div>"); 
+document.write("</nav>");
